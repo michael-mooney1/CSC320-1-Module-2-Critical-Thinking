bbd032766017d862233ac5883bcdda1ed280279c
Michael Mooney <michael.mooney1@csuglobal.edu>
2025-07-27 15:13:52 -0600

    Added ability to take 4 weekly incomes and calculate weekly and average tax amounts

--- src/Module3CriticalThinking/Module3CriticalThinking.java
+++ src/Module3CriticalThinking/Module3CriticalThinking.java
@@ -1,31 +1,61 @@
 package Module3CriticalThinking;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Module3CriticalThinking {
 	
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
-		
-		double weeklyTax = 0.0;
-		
-		System.out.println("Enter your weekly income as a whole number:");
-		
-		int weeklyIncome = scnr.nextInt();
-		
-		if (weeklyIncome < 0) {
-			System.out.println("Your weekly income needs to be a positive whole number.");
-			return;
-		} else if (weeklyIncome < 500) {
-			weeklyTax = weeklyIncome * 0.1;
-		} else if (weeklyIncome < 1500) {
-			weeklyTax = weeklyIncome * 0.15;
-		} else if (weeklyIncome < 2500) {
-			weeklyTax = weeklyIncome * 0.2;
-		} else {
-			weeklyTax = weeklyIncome * 0.3;
+		// Setup variables
+		ArrayList<Integer> weeklyIncomeList = new ArrayList<Integer>();
+		ArrayList<Double> weeklyTaxList = new ArrayList<Double>();
+		int weeklyIncome;
+		double weeklyTax;
+		double totalTax = 0.0;
+		double avgWeeklyTax;
+
+		// Get user input for weekly incomes
+		while (weeklyIncomeList.size() < 4) {
+			System.out.println("Enter your weekly income as a whole number:");
+			
+			weeklyIncome = scnr.nextInt();
+			if (weeklyIncome < 0) {
+				System.out.println("Your weekly income needs to be a positive whole number.");
+				continue;
+			}
+			weeklyIncomeList.add(weeklyIncome);
 		}
 		
-		System.out.println("The weekly tax on your weekly income is: " + weeklyTax);
+		// Calculate each weekly tax amount
+		for (int weeklyIncomeItem : weeklyIncomeList) {
+			if (weeklyIncomeItem < 500) {
+				weeklyTax = weeklyIncomeItem * 0.1;
+			} else if (weeklyIncomeItem < 1500) {
+				weeklyTax = weeklyIncomeItem * 0.15;
+			} else if (weeklyIncomeItem < 2500) {
+				weeklyTax = weeklyIncomeItem * 0.2;
+			} else {
+				weeklyTax = weeklyIncomeItem * 0.3;
+			}
+			weeklyTaxList.add(weeklyTax);
+		}
+		
+		// Calculate total tax amount
+		for (double weeklyTaxItem : weeklyTaxList) {
+			totalTax += weeklyTaxItem;
+		}
+		
+		// Calculate average tax amount
+		avgWeeklyTax = totalTax / 4;
+		
+		// Output weekly incomes and tax amounts
+		for (int i = 0; i < 4; i++) {
+			System.out.println("Week " + (i + 1) + " income: " + weeklyIncomeList.get(i));
+			System.out.println("Week " + (i + 1) + " tax: " + weeklyTaxList.get(i));
+		}
+		
+		// Output average weekly tax amount
+		System.out.println("The weekly tax on your weekly income is: " + avgWeeklyTax);
 	}
 }
